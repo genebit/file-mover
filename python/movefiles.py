41a32dabdf424dbc20910b89224950445e305c42
genebit <genebitara@gmail.com>
2021-05-30 19:51:10 +0800

    Project Finished 🎉🎉🎉
    + Managed to get the code working automatically once you run the code
    + Also understood basic knowledge in the watchdog python module

--- python/movefiles.py
+++ python/movefiles.py
@@ -1,13 +1,41 @@
 import os
 import shutil
+import time
+from watchdog import observers 
 
-destination_file = './dest/'
-download_path = 'C:\\Users\\BitaraPC\\Downloads\\'
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
 
-for file in os.listdir(download_path):
-    path, x = os.path.splitext(download_path)
-    name, extension = os.path.splitext(file)
+class MyHandler(FileSystemEventHandler):
     
-    if extension == '.txt':
-        absolute_path = path + name + extension
-        shutil.move(absolute_path, destination_file + 'txt/')        
+    def on_any_event(self, event):
+        print('Changes to the directory detected')
+        for file in os.listdir(downloads_folder):
+            name, extension = os.path.splitext(file)
+            absolute_path = (downloads_folder + name + extension)
+            
+            # Folder for each extension
+            txt_folder = '.txt/'
+    
+            if extension == '.txt':
+                print('Moving {0} in the txt folder'.format(file))
+                shutil.move(absolute_path, destination_folder + txt_folder)
+
+            elif extension != '.ini':
+                print('There are no files to move yet')
+
+
+downloads_folder = 'C:/Users/Gene/Downloads/'
+destination_folder = './dest/'
+
+event_handler = MyHandler()
+observer = Observer()
+observer.schedule(event_handler, downloads_folder, recursive=True)
+observer.start()
+
+try:
+    while True:
+        time.sleep(5) 
+except KeyboardInterrupt:
+    observer.stop()
+observer.join()
